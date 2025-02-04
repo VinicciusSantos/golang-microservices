e4de1327c014f9f4126cdbe5eb445e132ba9bda1
Vinicius Guedes <vinicius.guedes@grupobrisanet.com.br>
2025-02-04 14:06:37 -0300

    feat: updatinh the broker for a standart JSON format, and conect to auth service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,12 +1,87 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 )
+
+type requestPayload struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+}
+
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	app.writeJSON(w, http.StatusOK, jsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
+	})
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayload requestPayload
+
+	if err := app.readJSON(w, r, &requestPayload); err != nil {
+		app.errorJSON(w, err, http.StatusBadRequest)
+		return
+	}
+
+	switch requestPayload.Action {
+	case "auth":
+		app.authenticate(w, requestPayload.Auth)
+	default:
+		app.errorJSON(w, errors.New("unknown action"), http.StatusBadRequest)
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	var (
+		jsonData, _  = json.MarshalIndent(a, "", "\t")
+		client       = &http.Client{}
+		request      *http.Request
+		response     *http.Response
+		authResponse jsonResponse
+		err          error
+	)
+
+	if request, err = http.NewRequest(http.MethodPost, "http://authentication-service/authenticate", bytes.NewBuffer(jsonData)); err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if response, err = client.Do(request); err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"), http.StatusUnauthorized)
+		return
+	} else if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("authentication service error"), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewDecoder(response.Body).Decode(&authResponse); err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if authResponse.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	app.writeJSON(w, http.StatusOK, jsonResponse{
+		Error:   false,
+		Message: "Authenticated",
+		Data:    authResponse.Data,
 	})
 }
